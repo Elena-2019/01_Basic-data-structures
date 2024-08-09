6d6b42a5f31a5bb089c834f7c73eb395a9574981
Elena-2019 <loy-iver@inbox.ru>
2024-08-10 00:25:56 +0300

    Update 01_Базовые структуры данных.py

--- 01_Базовые структуры данных.py
+++ 01_Базовые структуры данных.py
@@ -2,8 +2,6 @@ print (9**0.5*5) #1st program
 
 print(9.99>9.98,1000!=1000.1) #2nd program
 
-print(2*2+2) #3rd program
-print(2*(2+2))
-print(6==8)
+print(2*2+2==2*(2+2)) #3rd program
 
-print(int(float('123.456')*10-1230.56))  #4th program
+print(int(float('123.456')*10%10)) #4th program
